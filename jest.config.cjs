21a879aed01308e9f40e81b47724305d07eeb4af
MichelleKamau6 <michwk06@gmail.com>
2025-07-29 13:41:36 +0300

    docs4

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.js'],
+  extensionsToTreatAsEsm: ['.jsx'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
